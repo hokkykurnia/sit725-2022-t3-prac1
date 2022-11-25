3199c9fe0071b58cc8049aeb8a1db3adbcd87629
hokkykurnia <hokky.kurnia@gmail.com>
2022-11-25 21:57:37 +1100

    updated

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function changeText(){​
+    var textsArray = ["Alpha","Bravo", "Charlie", "Delta", "Echo"]​
+    var number = getRandomNumberBetween(0,textsArray.length - 1)​
+    console.log("Index: ", number)​
+    document.getElementById("heading").innerHTML = textsArray[number];​
+}​
+
+​
+
+function getRandomNumberBetween(min,max){​
+    return Math.floor(Math.random()*(max-min+1)+min);​
+
+}​
